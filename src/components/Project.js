3ba5f5eb80a8975ca14489dcae370a25060823c9
Ajay K <ajaycoolkarni@yahoo.com>
2022-01-29 00:30:18 +1030

    projects populated

---
+++ src/components/Project.js
@@ -0,0 +1,65 @@
+// titled images of eight of the developer’s applications with links to both the deployed applications and the corresponding GitHub repository
+
+import React from "react";
+
+function Project(props) {
+  return (
+    <div>
+      <div className="columns is-desktop is-justify-content-center is-flex-wrap-wrap is-flex-direction-row">
+        {props.projects.map((project) => (
+          <div className="column is-half">
+            <div className="card">
+              <div className="card-image">
+                <figure className="image is-fullwidth">
+                  <a href={project.live} target="_blank" rel="noreferrer">
+                    <img src={process.env.PUBLIC_URL + project.image} alt="Placeholder image" />
+                  </a>
+                </figure>
+              </div>
+              <div className="card-content">
+                <div className="media">
+                  <div className="media-left"></div>
+                  <div className="media-content">
+                    <p className="title is-4" key={project.id}>
+                      {project.title}
+                    </p>
+                  </div>
+                </div>
+
+                <div className="content has-text-left">
+                  {project.description}
+                  <br />
+                  <br />
+                  <div className="content is-family-code">
+                    Technologies: {project.technologies}
+                  </div>
+                  <div className="card">
+                    <footer className="card-footer">
+                      <a
+                        href={project.repo}
+                        className="card-footer-item"
+                        target="_blank" rel="noreferrer"
+                      >
+                        Github
+                      </a>
+                      <br />
+                      <a
+                        href={project.live}
+                        className="card-footer-item"
+                        target="_blank" rel="noreferrer"
+                      >
+                        Demo/Live site
+                      </a>
+                    </footer>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Project;
